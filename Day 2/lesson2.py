54f5ed8c1e4dfdda80bf6dfed0b5b92eeb52d3a8
Shwartswald <parna.sartania@gmail.com>
2022-09-21 21:51:31 +0400

    Second Commit

--- Day 2/lesson2.py
+++ Day 2/lesson2.py
@@ -1,1 +1,30 @@
-print ("This is day two")
+from unicodedata import name
+
+
+name = "Parna"
+surname = "Sartania"
+print(name)
+print("zviad")
+
+age = 27
+height = 181.5
+
+profession = "DJ"
+
+
+
+knows_programming = False
+
+is_deejay = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(knows_programming))
+print(type(is_deejay))
+
+print(name + " " + surname)
+print(name + " " + str(age))
+
+
+print("me var" + " " + name + " " + surname + " " + str(age) + " Wlis" + " " + profession + " Chemi simaglea " + str(height) + " Cm" )
