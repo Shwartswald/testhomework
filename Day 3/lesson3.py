7f608013247f0b154f734447348ba199d2cfa76e
Shwartswald <parna.sartania@gmail.com>
2022-09-29 17:40:11 +0400

    fourth commit

---
+++ Day 3/lesson3.py
@@ -0,0 +1,60 @@
+
+
+
+name = "parna"
+#age = "27"
+#height = 183
+
+
+
+#print("Chemi Saxelia {} chemi asakia {} da simagle {} ".format(name, age, height))
+
+
+
+
+
+#message = """გამარჯობა, გაკვეთილი გვაქ 18 სექტემბერს ბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლაბლა
+#ბლაბლაბლაბლაბლაბლა
+#ბლაბლაბლაბლაბლაბლა
+#"""""
+
+#print(message)
+
+#print(name.capitalize())
+
+full_name = "parna sartania"
+
+#print(full_name[4])
+
+#print(full_name[3:8])
+#print(full_name[6:14])
+
+
+#print(len(name))
+
+#if "n" in full_name:
+#    print("gxvdeba")
+
+#if "k" not in full_name:
+#    print("ar gxvdeba")
+
+
+#print(full_name[2:])
+#print(full_name[:7])
+
+#print(full_name[::2])
+
+
+#print(full_name[-1])
+
+#print(full_name[7:3:-1])
+
+#print(full_name.upper())
+
+
+name2 = "    gio     "
+#print(name2.strip())
+
+
+print(name2.replace("g","n"))
+
---
+++ Day 4/lesson4.py
@@ -0,0 +1,101 @@
+
+#my_sentance = " aa {1} bb {2} cc {0} " .format("zz", "yy", "xx",)
+
+
+#print(my_sentance)
+
+
+#if 9>5:
+#    print("ki")
+#else:
+#    print("ara")
+
+
+# if False:
+#     print("10")
+# else:
+#     print("20")
+
+
+
+
+
+
+# age = 22 
+# age += 3
+
+# print(age)
+
+
+
+# my_age = input("how old are you: ")
+
+# print ("i am " + my_age + " years old ")
+
+
+
+# ricxi1 = input ("pirveli ricxvi:")
+# ricxvi2 = input ("meore ricxvi:" )
+
+# print("jami =" + str(int(ricxi1)+int(ricxvi2)))
+
+
+
+# ricxi1 = int(input ("pirveli ricxvi:"))
+# ricxvi2 = int(input ("meore ricxvi:" ))
+
+# print("jami =" + str((ricxi1)+(ricxvi2)))
+
+
+
+
+
+# num1 = int(input("pirveli ricxvi: "))
+# num2 = int(input("meore ricxvi: "))
+
+# product = num1 * num2
+
+# if product > 100:
+#     print ("xxx")
+# else:
+#     print("yyy")
+
+
+
+
+
+# a = 10
+# b = 23
+
+# print(a + b)
+# print(a * b)
+# print(a / b)
+# print(a - b)
+
+
+# print(b % a)
+# print(a ** b)
+# print(a//b)
+
+
+
+
+
+
+
+
+num1 = int(input("pirveli ricxvi: "))
+num2 = int(input("meore ricxvi: "))
+num3 = int(input("mesame ricxvi: "))
+sum = 0
+
+
+if (num1 % 2) == 1:
+    sum=sum + num1
+if (num2 % 2) == 1:
+    sum = sum + num2
+if (num3 % 2) == 1:
+    sum = sum + num3
+print (sum)
+
+
